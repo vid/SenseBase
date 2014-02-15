7949b496818d1ddf2282a4d6a2932b9ca4aa1f1e
Vid <vid_github@zooid.org>
2014-02-15 12:48:22 -0500

    fix arg

--- host/schema.sh
+++ host/schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "e$1" == "e" ]; then echo "usage: $0 <indexname>"; exit 1; fi
+if [ $# -eq 0 ]; then echo "usage: $0 <indexname>"; exit 1; fi
 
 curl -XDELETE http://localhost:9200/$1/
 curl -XPUT http://localhost:9200/$1/
@@ -23,4 +23,24 @@ curl -XPUT "http://localhost:9200/$1/contentItem/_mapping" -d '
   }
 }'
 
+curl -XPUT "http://localhost:9200/$1/removedItem/_mapping" -d '
+{
+  "removedItem" : {
+      "_id" : {
+        "path" : "uri",
+        "index": "not_analyzed", "store" : "yes"
+      },
+    "properties" : {
+      "@timestamp" : {
+        "type" : "date",
+        "format" : "dateOptionalTime"
+      },
+      "uri" : {
+        "type" : "string", "index":"not_analyzed", "store" : "yes"
+      }
+    }
+  }
+}'
+
 curl -XGET http://localhost:9200/$1/_mapping?pretty=true
+
