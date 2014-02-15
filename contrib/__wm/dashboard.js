4c8d45a2a49db462ef904579df5beac7f9bde8e9
Vid <vid_github@zooid.org>
2014-02-14 21:21:53 -0500

    simplify layout

--- contrib/__wm/dashboard.js
+++ contrib/__wm/dashboard.js
@@ -1,13 +1,13 @@
 var pxMember;
 
 $(function() {
-  $('.main.menu .item').tab('change tab', 'second'); // FIXME to select second menu
   $('.ui.accordion').accordion();
   $('#dashboardLink').click(function() { $('.details.sidebar').sidebar('toggle'); return false;});
   $('.details.sidebar').sidebar('hide', { overlay: true});
   $('.facets.sidebar').sidebar('hide');
   $('.ui.facets.button').click(function() { $('.facets.sidebar').sidebar('toggle'); });
   $('.ui.scrape.button').click(function() { $('.scrape.content').toggle('hidden'); });
+  $('.ui.manage.button').click(function() { $('.manage.content').toggle('hidden'); });
   $('.sortable.table').tablesort();
   $('.ui.checkbox').checkbox({onChange : updateOptions});
 
