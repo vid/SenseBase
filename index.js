a59898fd1062a061aad5c5a4ab59cb1ff063df5e
Vid <vid_github@zooid.org>
2014-02-15 19:58:41 -0500

    changing to new annotations

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// main module for SenseBase
+// meant to be modular but that needs some work.
 
 var fs = require('fs'), 
   http = require('http'),
@@ -7,14 +9,13 @@ var fs = require('fs'),
   util = require('util'),
   LocalStrategy = require('passport-local').Strategy;
   
-var fileUpload = require('./lib/file-upload.js');
+var fileUpload = require('./lib/file-upload.js'), pubsub;
 
 GLOBAL.authed = GLOBAL.authed || {}; //FIXME  use auth scheme that works behind proxies
 var users = require('./users.json').logins;
 
-exports.start = start;
-
-function start(config) {
+// start server with a configuration file
+exports.start = function(config) {
   config.users = users;
   config.indexer = require('./lib/indexer.js');
   config.pageCache = require('./lib/pageCache.js');
@@ -32,150 +33,157 @@ function start(config) {
         var psMember = browser_request.psMember.username;
         GLOBAL.config.pageCache.cache(uri, referer, is_html, pageBuffer, contentType, saveHeaders, browser_request);
         GLOBAL.config.indexer.saveContentItem({ uri: uri, title: title, member: psMember, referer: referer, isHTML: browser_request.is_html, content: pageBuffer, contentType: contentType, headers: saveHeaders});
+        requestAnnotate({uri: uri, content: pageBuffer});
       }
     }
   }
   // globally shared context
   GLOBAL.config = config;
+  pubsub = require('./lib/pubsub.js');
 
-// Passport stuff
-function findById(id, fn) {
-  var found = null;
-  users.forEach(function(u) {
-    if (u.id == id) {
-      found = u;
-    }
-  });
-  if (found) {
-    fn(null, found);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification
-    process.nextTick(function () {
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        if (!user.active) { return done(null, false, { message: 'Account not active' }); }
-        return done(null, user);
-      });
+  // Passport stuff
+  function findById(id, fn) {
+    var found = null;
+    users.forEach(function(u) {
+      if (u.id == id) {
+        found = u;
+      }
     });
+    if (found) {
+      fn(null, found);
+    } else {
+      fn(new Error('User ' + id + ' does not exist'));
+    }
   }
-));
-
-// Express stuff
-var app = express();
-
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.cookieParser());
-  app.use(express.logger({stream: GLOBAL.config.logStream}));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'popsicle-fish' }));
-  // Initialize Passport!  Also use passport.session() middleware, to support persistent login sessions (recommended).
-  app.use(flash());
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static('static'));
-  app.enable('trust proxy');
-});
-
-app.all('/__wm/*', function(req, res, next) {
-  if (req.user) {
-//console.log('USER', req.user);
-    next();
-  } else {
-    res.redirect("/login"); 
+  
+  function findByUsername(username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+      var user = users[i];
+      if (user.username === username) {
+        return fn(null, user);
+      }
+    }
+    return fn(null, null);
   }
-});
-
-app.get('/', function(req, res){
-console.log('OO', req.session.user);
-  if (req.user) {
-    res.cookie('psMember', req.user.username, { maxAge: 900000 });
-    res.cookie('psSession', req.user.id + '/' + new Date().getTime(), { maxAge: 900000 });
-     
-    res.redirect('/__wm/index.html');
-//    res.render('index', { user: req.user.username });
-  } else {
-    res.render('login', { user: req.user, message: req.flash('error') });
-  }
-});
-
-// used by client for identity
-app.get('/member.js', function(req, res) {
-  res.render('memberjs', { user: req.user });
-});
-
-app.get('/login', function(req, res){
-  res.render('login', { user: req.user, message: req.flash('error') });
-});
-
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res) {
-    GLOBAL.authed[req.ip] = req.user;
-    console.log('OOOOOOO', req.ip, GLOBAL.authed);
-    var oreq = req.headers['original-request'] || '/';
-    if (oreq == 'undefined') { oreq = '/'; }
-    res.redirect(GLOBAL.config.HOMEPAGE);
+  
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
   });
   
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-app.post('/upload', function(req, res) {
-  fileUpload.uploadFile(req, function(err, resp) {
-    GLOBAL.config.indexer.saveContentItem({ uri: GLOBAL.config.HOMEPAGE + '/files/' + resp.fileName, isHTML: true, title: resp.title, member: req.user.username, content: resp.buffer || 'NOCONTENT'}, function(err, res, esDoc) {
-      pubsub.updateItem(esDoc);
-      console.log('uploaded', resp.fileName);
-
+  passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+      done(err, user);
     });
   });
-  res.end();
-});
-
-var server = app.listen(9999);
-
-var pubsub = require('./lib/pubsub.js');
-pubsub.start(server);
-
-var filterProxy = require('filter-proxy');
-
-filterProxy.start(GLOBAL.config); 
-
-// interactive command line
-
-repl = require("repl");
-r = repl.start({ prompt: GLOBAL.config.project + "> ", useGlobal: true});
+  
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      // asynchronous verification
+      process.nextTick(function () {
+        findByUsername(username, function(err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+          if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+          if (!user.active) { return done(null, false, { message: 'Account not active' }); }
+          return done(null, user);
+        });
+      });
+    }
+  ));
+  
+  // Express stuff
+  var app = express();
+  
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.cookieParser());
+    app.use(express.logger({stream: GLOBAL.config.logStream}));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.session({ secret: 'popsicle-fish' }));
+    // Initialize Passport!  Also use passport.session() middleware, to support persistent login sessions (recommended).
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
+    app.use(express.static('static'));
+    app.enable('trust proxy');
+  });
+  
+  app.all('/__wm/*', function(req, res, next) {
+    if (req.user) {
+  //console.log('USER', req.user);
+      next();
+    } else {
+      res.redirect("/login"); 
+    }
+  });
+  
+  app.get('/', function(req, res){
+  console.log('OO', req.session.user);
+    if (req.user) {
+      res.cookie('psMember', req.user.username, { maxAge: 900000 });
+      res.cookie('psSession', req.user.id + '/' + new Date().getTime(), { maxAge: 900000 });
+       
+      res.redirect('/__wm/index.html');
+  //    res.render('index', { user: req.user.username });
+    } else {
+      res.render('login', { user: req.user, message: req.flash('error') });
+    }
+  });
+  
+  // used by client for identity
+  app.get('/member.js', function(req, res) {
+    res.render('memberjs', { user: req.user });
+  });
+  
+  app.get('/login', function(req, res){
+    res.render('login', { user: req.user, message: req.flash('error') });
+  });
+  
+  app.post('/login', 
+    passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+    function(req, res) {
+      GLOBAL.authed[req.ip] = req.user;
+      console.log('OOOOOOO', req.ip, GLOBAL.authed);
+      var oreq = req.headers['original-request'] || '/';
+      if (oreq == 'undefined') { oreq = '/'; }
+      res.redirect(GLOBAL.config.HOMEPAGE);
+    });
+    
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+  
+  app.post('/upload', function(req, res) {
+    fileUpload.uploadFile(req, function(err, resp) {
+      GLOBAL.config.indexer.saveContentItem({ uri: GLOBAL.config.HOMEPAGE + '/files/' + resp.fileName, isHTML: true, title: resp.title, member: req.user.username, content: resp.buffer || 'NOCONTENT'}, function(err, res, cItem) {
+        console.log('uploaded', resp.fileName);
+        pubsub.updateItem(cItem);
+        requestAnnotate(cItem.uri);
+      });
+    });
+    res.end();
+  });
+  
+  var server = app.listen(9999);
+  
+  pubsub.start(server);
+  
+  var filterProxy = require('filter-proxy');
+  
+  filterProxy.start(GLOBAL.config); 
+  
+  // interactive command line
+  
+  repl = require("repl");
+  r = repl.start({ prompt: GLOBAL.config.project + "> ", useGlobal: true});
+  }
+  
+// request subscribed annotators to annotate
+function requestAnnotate(cItem) {
+   pubsub.requestAnnotate(cItem);
 }
+
--- test/annotations.js
+++ test/annotations.js
@@ -2,11 +2,26 @@
 
 var annotations = require('../lib/annotations.js');
 
-var range = annotations.createRange({exact: 'test exact', offset: 20, range: 200});
-var annoRange = annotations.createAnnotation({annotatedBy: 'test', type: 'quote', range: range});
-var item = annotations.createContentItem({title: 'test title', uri: 'http://test.com/', content: 'test content', annotations: [annoRange]});
-var annoTag = annotations.createAnnotation({annotatedBy: 'test', type: 'tag', tag: 'test tag'});
-annotations.addAnnotation(item, annoTag);
+var item = annotations.createContentItem({title: 'test title', uri: 'http://test.com/', content: 'test content'});
+console.log('item', item);
 
-console.log(JSON.stringify(item, null, 2));
+var annoCategory = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'category', category: 'test category'});
+console.log('annoCategory', annoCategory);
 
+var annoValue = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'value', key: 'test key', value: 'test value'});
+console.log('annoValue', annoValue);
+
+var quoteRange = annotations.createRange({exact: 'quote exact', offset: 20, range: 200});
+var annoRange = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'quote', quote: 'test', range: quoteRange});
+console.log('annoRange', annoRange);
+
+var valueRange = annotations.createRange({exact: 'value exact', offset: 20, range: 200});
+var annoValueQuote = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'valueQuote', key: 'test key', value: 'test value', range: valueRange});
+console.log('annoValueQuote', annoValueQuote);
+
+var annoLib = require('../lib/annotateServices/annotateLib');
+
+var text = '<html><body>This is a match1 and a match2. Here is match1 again.</body></html>';
+var range = annoLib.rangesFromMatches('match1', text);
+
+console.log('range', range);
