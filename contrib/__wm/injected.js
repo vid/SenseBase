351717525e004a7a1c587ff76faf78dd22298aa0
Vid <vid_github@zooid.org>
2014-03-26 18:08:27 -0400

    capture previous onload

--- contrib/__wm/injected.js
+++ contrib/__wm/injected.js
@@ -8,9 +8,16 @@ function createDiv() {
       document.body.innerHTML = '<div id="sbEnclosure">' + document.body.innerHTML + '</div>';
     }
     */
-    document.body.innerHTML += '<div id="sbIframe" style="z-index: 999; position: fixed; right: 1em; top: 0; height: 90%; color: black; background: #ffe; filter:alpha(opacity=90); opacity:0.9; border: 0"><iframe style="width: 100%; height: 100%" src="/__wm/injected-iframe.html"></div></div>';
+    document.body.innerHTML += '<style>.sbAnnotation { background: lightblue; }\n.sbEnclosure { border: 1px solid lightblue; padding: 0px; margin: 0px;}</style><div id="sbIframe" style="z-index: 999; position: fixed; right: 1em; top: 0; height: 90%; color: black; background: #ffe; filter:alpha(opacity=90); opacity:0.9; border: 0"><iframe style="width: 100%; height: 100%" src="/__wm/injected-iframe.html"></div></div>';
   } else {
     console.log('NOT injecting SenseBase iframe');
   }
 }
-window.onload = createDiv;
+
+onloadPrevious = document.onload;
+window.onload = function () {
+  if (typeof onloadPrevious == "function") {
+    onloadPrevious();
+  }
+  createDiv();
+}
