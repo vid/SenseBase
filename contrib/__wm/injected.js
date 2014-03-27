b12a677a5a77a11154c8fa01318c033fb4c03d4d
Vid <vid_github@zooid.org>
2014-03-27 13:19:23 -0400

    unused code

--- contrib/__wm/injected.js
+++ contrib/__wm/injected.js
@@ -1,16 +1,9 @@
 function createDiv() { 
   if (!parent.document.getElementById('sbIframe')) {
     console.log('injecting SenseBase iframe');
-    /*
-    // we need at least one class or div to work with
-    if (document.body.innerHTML.indexOf(' class=') < 0 && document.body.innerHTML.indexOf(' id=') < 0) {
-      console.log('injecting base enclosure');
-      document.body.innerHTML = '<div id="sbEnclosure">' + document.body.innerHTML + '</div>';
-    }
-    */
     document.body.innerHTML += '<style>.sbShort { height: 10%; }\n.sbAnnotation { background: lightblue; }\n.sbEnclosure { border: 1px solid lightblue; padding: 0px; margin: 0px;}</style><div id="sbIframe" style="z-index: 999; position: fixed; right: 1em; top: 0; height: 90%; color: black; background: #ffe; filter:alpha(opacity=90); opacity:0.9; border: 0"><iframe style="width: 100%; height: 100%" src="/__wm/injected-iframe.html"></div></div>';
   } else {
-    console.log('NOT injecting SenseBase iframe');
+    console.log('NOT nesting SenseBase iframe');
   }
 }
 
