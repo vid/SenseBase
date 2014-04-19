ca60b58f4e314239f75fbbd112206941ad9c3e1b
Vid <vid_github@zooid.org>
2014-04-19 11:31:07 -0400

    simple structure annotator

---
+++ test/annotator-sentiment.js
@@ -0,0 +1,17 @@
+
+var expect = require("expect.js"), indexer = require('../lib/indexer.js'), fs = require('fs'), path = require('path');
+GLOBAL.config = require('../config.js').config;
+var sentiment = require('../lib/annotators/sentiment.js');
+
+var doc = '<html><script lah lah></script><body class="something">Good <b>bad</b> amazing</body></html>';
+
+describe('sentiment', function(done){
+  it('should identify the candidates', function() {
+    sentiment.process({ uri: 'test', html: doc, text: doc.replace(/<.*?>/g, '')}, function(err, result) {
+      console.dir('EE', err, result);
+      expect(err).to.be.undefined;
+      done();
+    });
+  });
+});
+
--- test/annotator-structured.js
+++ test/annotator-structured.js
@@ -3,7 +3,7 @@ var fs = require('fs'), expect = require("expect.js"), indexer = require('../lib
 GLOBAL.config = require('../config.js').config;
 var annotatorStructural = require('../lib/annotators/structural.js');
 
-var sampleDoc = '', i = 0;
+var sampleDoc = fs.readFileSync('./data/structured-medical-record.html'), i = 0;
 var fields = ['Date of Admission', 'Date of Discharge to Home', 'Admitting Diagnosis', 'Discharge Diagnosis', 'Discharge Condition', 'Consults', 'Procedures', 'Brief History of Present Illness', 'Hospital Course', 'Physical Examination at Discharge', 'Medications', 'Activity', 'Diet', 'Follow Up', 'Instructions'];
 fields.forEach(function(f) {
   sampleDoc += f + ': ' + i++ + '\n';
@@ -11,10 +11,10 @@ fields.forEach(function(f) {
 
 describe('Medical report', function(done){
   it('should extract the fields', function(done) {
-    annotatorStructural.process(sampleDoc, function(err, result) {
-      console.log('EE', err, result);
+    annotatorStructural.process({ uri: GLOBAL.homepage + '/test', text: sampleDoc}, function(err, result) {
       expect(err).to.be.undefined;
-      expect(result && result.length === fields.length).to.be(true);
+      var l = result.length;
+      expect(l).to.be(14);
       done();
     });
   });
