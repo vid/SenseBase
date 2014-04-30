6627ad72a2a6309388684e527b0fe95cddf957d0
Vid <vid_github@zooid.org>
2014-04-30 13:10:47 -0400

    simplify annotation displa

--- contrib/__wm/injected-iframe.js
+++ contrib/__wm/injected-iframe.js
@@ -1,190 +1,154 @@
 (function() {
-  var lastPlaced, blinkAnno, selectMode = false, displayAll = true, lastSelected;
+  var selectMode = false;
+  // html buffer while annotation tags are processed
+  var newHTML;
   var treeInterface = { 
     hover: function(anno) {
       if (!selectMode) { 
-        displayAnno(anno, displayAll);
+console.log('hover', anno);
       } 
     }, 
     select: function(anno) {
-
-    // clear select mode if it's the same anno
-    if (selectMode && lastSelected === anno) {
-      selectMode = false;
-    } else {
-      selectMode = true;
-      displayAnno(anno, displayAll);
-    }
+      // clear select mode if it's the same anno
+      if (selectMode && lastSelected === anno) {
+        selectMode = false;
+      } else {
+        selectMode = true;
+console.log('select', anno);
+      }
     }
   };
-  ready();
-  function ready() {
-    $('#sbIframe', parent.document).after('<div id="sbAnnotationDetails" style="background: #ffe; filter:alpha(opacity=90); opacity:0.9; position: absolute; top: 8%; left: 8%; width: 80%; height: 80%; display: none; z-index: 999; border: 1px dotted grey"><i class="close icon"></i><pre></pre></div>');
-    $('#sbIframe', parent.document).after('<style>\n.sbShort { height: 10%; }\n.sbAnnotationBlink { background: yellow !important; }\n.sbAnnotation-a { background: lightgreen; }\n.sbAnnotation-b { background: lightblue; }\n</style>');
-    var fayeClient = new Faye.Client('<!-- @var FAYEHOST -->');
-  // do scraper actions as appropriate
-    var senseBase = window.senseBase;
-    if (senseBase.isScraper) {
-      $('#treeContainer').html('<h1>scraper</h1>');
-      console.log('isScraper', senseBase);
-      // publish our current link
-      setInterval(function() {
-        console.log('scraper waiting for link');
-        fayeClient.publish('/visitWait');
-      }, 20000);
-      fayeClient.subscribe('/visit', function(msg) {
-        $('#treeContainer').html(JSON.stringify(msg, null, 2));
-        console.log('/visit', msg);
-        if (!msg.site || !msg.site.uri || msg.site.uri.toLowerCase().indexOf('https') === 0) {
-          console.log('not scraping undefined');
+
+  $('#sbIframe', parent.document).after('<div id="sbAnnotationDetails" style="background: #ffe; filter:alpha(opacity=90); opacity:0.9; position: absolute; top: 8%; left: 8%; width: 80%; height: 80%; display: none; z-index: 999; border: 1px dotted grey"><i class="close icon"></i><pre></pre></div>');
+  $('#sbIframe', parent.document).after('<style>\n.sbShort { height: 10%; }\n.sbAnnotationBlink { background: yellow !important; }\n.sbAnnotation-a { background: lightgreen; }\n.sbAnnotation-b { background: lightblue; }\n</style>');
+  var fayeClient = new Faye.Client('<!-- @var FAYEHOST -->');
+// do scraper actions as appropriate
+  var senseBase = window.senseBase;
+  if (senseBase.isScraper) {
+    $('#treeContainer').html('<h1>scraper</h1>');
+    console.log('isScraper', senseBase);
+    // publish our current link
+    setInterval(function() {
+      console.log('scraper waiting for link');
+      fayeClient.publish('/visitWait');
+    }, 20000);
+    fayeClient.subscribe('/visit', function(msg) {
+      $('#treeContainer').html(JSON.stringify(msg, null, 2));
+      console.log('/visit', msg);
+      if (!msg.site || !msg.site.uri || msg.site.uri.toLowerCase().indexOf('https') === 0) {
+        console.log('not scraping undefined');
+      } else {
+        if (msg.site.uri === parent.window.location.href) {
+          console.log('not re-indexing self');
         } else {
-          if (msg.site.uri === parent.window.location.href) {
-            console.log('not re-indexing self');
-          } else {
-            setTimeout(function() { parent.window.location.href = msg.site.uri;}, 2000);
-          }
-        }
-      });
-      return;
-    }
-
-    $('body').append('<span id="annotationCount"></span><div id="treeContainer"></div>');
-    $('head').append('<link rel="stylesheet" href="<!-- @var HOMEPAGE -->/lib/jstree/dist/themes/default/style.min.css" />');
-    $('head').append('<link rel="stylesheet" href="<!-- @var HOMEPAGE -->/lib/Semantic-UI/build/packaged/css/semantic.css" />');
-
-    // actions
-    $('.left.hand.icon').click(function() {
-      var w = 300; //window.innerwidth;
-      $('#sbIframe', parent.document).css('left', '1em')
-      $('#sbIframe', parent.document).css('width', w + 'px');
-    });
-    $('.long.arrow.right.icon').click(function() {
-      console.log('embed');
-      $('#sbIframe', parent.document).prependTo('#SBInsie', parent.document);
-    });
-
-    $('.refresh.icon').click(function() {
-      console.log('updateContent');
-      fayeClient.publish('/updateContent', { uri: parent.window.location.href, content: parent.document.documentElement.outerHTML} );
-    });
-
-    $('.minus.checkbox.icon').click(function() {
-      // FIXME
-      $('#sbIframe', parent.document).css('height', $('#sbIframe', parent.document).css('height') === '50px' ? '90%' : '50px');
-      console.log('toggle short');
-    });
-    console.log('SenseBase iframe', parent.window.location.href);
-    fayeClient.publish('/annotate', { uri: parent.window.location.href} );
-
-    fayeClient.subscribe('/annotations', function(data) {
-      var annotations = data.annotations, uri = data.uri;
-      if (uri.replace(/#.*/, '') !== parent.window.location.href) {
-        console.log('ignoring annotations', uri);
-        return;
-      }
-      console.log('/annotations', data);
-
-      var treeItems = displayAnnoTree(annotations, uri, treeInterface);
-      displayAllAnnos(treeItems);
-      $('.sbAnnotation', parent.document).click(function() {
-        $('#sbAnnotationDetails', parent.document).show();
-        $('#sbAnnotationDetails pre', parent.document).text(JSON.stringify(treeItems.get($(this).attr('id').split('-')[2]), null, 2));
-        $('#sbAnnotationDetails', parent.document).click(function() { $(this).hide();} ); 
-      });
-    });
-
-    function displayAllAnnos(treeItems) {
-      // sort and display annotations, longest first
-      var items = $.extend({}, treeItems.map), longest = 1;
-      while (longest > 0) {
-        longest = 0;
-        for (var id in items) {
-          if (items[id].exact) {
-            if (items[id].exact.length > longest) {
-              longest = parseInt(id, 10);
-            }
-          } else {
-            delete items[id];
-          }
-        }
-        if (items[id]) {
-          displayAnno(items[id], !displayAll);
-          delete items[id];
+          setTimeout(function() { parent.window.location.href = msg.site.uri;}, 2000);
         }
       }
-    }
-
+    });
+    return;
   }
-  // add annotation tags, creating single if singleDisplay is true
-  function displayAnno(anno, singleDisplay) {
-    // it's not an instance
-    if (!anno) {
+
+  $('body').append('<span id="annotationCount"></span><div id="treeContainer"></div>');
+  $('head').append('<link rel="stylesheet" href="<!-- @var HOMEPAGE -->/lib/jstree/dist/themes/default/style.min.css" />');
+  $('head').append('<link rel="stylesheet" href="<!-- @var HOMEPAGE -->/lib/Semantic-UI/build/packaged/css/semantic.css" />');
+
+  // actions
+  $('.left.hand.icon').click(function() {
+    var w = 300; //window.innerwidth;
+    $('#sbIframe', parent.document).css('left', '1em')
+    $('#sbIframe', parent.document).css('width', w + 'px');
+  });
+  $('.long.arrow.right.icon').click(function() {
+    console.log('embed');
+    $('#sbIframe', parent.document).prependTo('#SBInsie', parent.document);
+  });
+
+  $('.refresh.icon').click(function() {
+    console.log('updateContent');
+    fayeClient.publish('/updateContent', { uri: parent.window.location.href, content: parent.document.documentElement.outerHTML} );
+  });
+
+  $('.minus.checkbox.icon').click(function() {
+    // FIXME
+    $('#sbIframe', parent.document).css('height', $('#sbIframe', parent.document).css('height') === '50px' ? '90%' : '50px');
+    console.log('toggle short');
+  });
+  console.log('SenseBase iframe', parent.window.location.href);
+  fayeClient.publish('/annotate', { uri: parent.window.location.href} );
+
+  fayeClient.subscribe('/annotations', function(data) {
+    var annotations = data.annotations, uri = data.uri;
+    if (uri.replace(/#.*/, '') !== parent.window.location.href) {
+      console.log('ignoring annotations', uri);
       return;
     }
-//    console.log('ANNO', anno, { single: singleDisplay});
-    // clear any last selected annotation
-    clearTimeout(blinkAnno);
-    $('.sbAnnotationBlink').removeClass('sbAnnotationBlink');
-    if (lastPlaced) {
-      $(lastPlaced.selector, parent.document).html(lastPlaced.replaced);
-      lastPlaced = undefined;
-      if (anno.placed) {
-        delete anno.placed;
+    console.log('/annotations', data);
+
+    var treeItems = displayAnnoTree(annotations, uri, treeInterface);
+    displayAllAnnos(treeItems);
+    $('.sbAnnotation', parent.document).click(function() {
+      $('#sbAnnotationDetails', parent.document).show();
+      $('#sbAnnotationDetails pre', parent.document).text(JSON.stringify(treeItems.get($(this).attr('id').split('-')[2]), null, 2));
+      $('#sbAnnotationDetails', parent.document).click(function() { $(this).hide();} ); 
+    });
+  });
+
+  // find the starting position of an instance
+  function findInstanceOffset(anno, text) {
+    var re = new RegExp('\\b'+anno.exact+'\\b', 'g'), cur = 1, match;
+    while ((match = re.exec(text)) !== null) {
+      if (cur === anno.instance) {
+        return match.index;
+      }
+      cur++;
+    }
+  }
+
+  // prepare and display eligible annotations
+  function displayAllAnnos(treeItems) {
+    var items = [], selector, newHTML;
+    // two passes; first assign offsets and add to array with the same selector
+    for (var i in treeItems.map) {
+      var anno = treeItems.map[i];
+      if (anno.instance && (!selector || selector === anno.selector)) {
+        if (!newHTML) {
+          selector = anno.selector;
+          if (selector === 'body') {
+            selector = '#SBEnclosure';
+          }
+          newHTML = $(selector, parent.document).html();
+        }
+        anno.offset = findInstanceOffset(anno, newHTML);
+        items.push(anno);
       }
     }
-
-    // exact string
-    if (anno.exact) {
-      if (!anno.selector) {
-        anno.selector = 'annoLocation';
-      }
-      var annoLocation = $(anno.selector, parent.document).html();
-
-      // add offset by instance (occurance)
-      if (anno.instance) {
-        var re = new RegExp('\\b'+anno.exact+'\\b', 'g'), cur = 1, match;
-        while ((match = re.exec(annoLocation)) !== null) {
-          if (cur === anno.instance) {
-            anno.offset = match.index;
-            break;
-          }
-          cur++;
-        }
-        if (!anno.offset) {
-          console.log(anno, 'was not found');
+    console.log(items.length, 'eligible in', selector, 'out of', i);
+    // second pass, sort and display annotations, later first.
+    var latest = 1, i, item;
+    while (latest > -1) {
+      latest = -1;
+      for (i = 0; i < items.length; i++) {
+        item = items[i];
+        if (item && item.offset > latest) {
+          latest = i;
         }
       }
-      if (anno.offset) {
-        // find the most specific enclosing element
-        var annoID = 'SB-anno-' + anno.__id;
-        // FIXME demo
-        var startTag = '<span id="' + annoID + '" class="sbAnnotation sbAnnotation-' + (anno.annotatedBy === 'AFINN Sentiment' ? 'a' : 'b') + '">', endTag = '</span>';
-        
-        if (anno.selector) {
-          if (!singleDisplay) {
-            var toReplace = $(anno.selector, parent.document).html();
-            // debugging
-            var t = toReplace.substr(anno.offset - 1, anno.exact.length);
-            // end debugging
-            $(anno.selector, parent.document).html(toReplace.substring(0, anno.offset) + 
-              startTag + 
-              anno.exact + 
-              endTag + 
-              toReplace.substring(anno.offset + anno.exact.length));
-            console.log('replaced', anno.exact);
-            var placed = { selector: anno.selector, replaced: toReplace}
-            anno.placed = placed;
-          } else {
-            lastPlaced = placed;
-          }
-          lastSelected = anno;
-          blinkAnno = setInterval(function() { $('#'+annoID, parent.document).toggleClass('sbAnnotationBlink')}, 500);
-//          console.log('found enclosing', { selection: $(anno.selector, parent.document).html(), placed: placed});
-        } else {
-          console.log('not found', anno.selector, 'for', anno);
-        }
+
+      if (latest > -1) {
+        newHTML = insertAnno(items[latest], newHTML);
+        delete items[latest];
       }
     }
+    $(selector, parent.document).html(newHTML);
+  }
+
+  // insert annotation tags at correct position
+  function insertAnno(anno, newHTML) {
+    var annoID = 'SB-anno-' + anno.__id;
+    var startTag = '<span id="' + annoID + '" class="sbAnnotation sbAnnotation-b">', endTag = '</span>';
+    
+    anno.offset = findInstanceOffset(anno, newHTML);
+    return newHTML.substring(0, anno.offset) + startTag + anno.exact + endTag + newHTML.substring(anno.offset + anno.exact.length);
   }
 include "displayAnnoTree.js"
 }());
