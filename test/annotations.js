9e73ae2cba4102f9eca7d29bf47cc3827ce117a4
Vid <vid_github@zooid.org>
2014-06-05 16:12:56 -0400

    convert to test

--- test/annotations.js
+++ test/annotations.js
@@ -1,27 +1,32 @@
-// sample code to use annotations (TODO: convert to test)
+// test annotation types
+// these tests rely on the library not throwing an exception.
 
-var annotations = require('../lib/annotations.js');
+var annotations = require('../lib/annotations.js'), annoLib = require('../lib/annotators/annotateLib');
 
 var item = annotations.createContentItem({title: 'test title', uri: 'http://test.com/', content: 'test content'});
-console.log('item', item);
 
-var annoCategory = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'category', category: 'test category'});
-console.log('annoCategory', annoCategory);
+describe('Annotations', function(done){
+  it('should create category annotation', function() {
+    var annoCategory = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'category', category: 'test category'});
+  });
 
-var annoValue = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'value', key: 'test key', value: 'test value'});
-console.log('annoValue', annoValue);
+  it('should create value annotation', function() {
+    var annoValue = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'value', key: 'test key', value: 'test value', selector: 'body'});
+  });
 
-var quoteRange = annotations.createRange({exact: 'quote exact', offset: 100});
-var annoRange = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'quote', quote: 'test', ranges: quoteRange});
-console.log('annoRange', annoRange);
+  it('should create annoRange annotation', function() {
+    var quoteRange = annotations.createRange({exact: 'quote exact', offset: 100, selector: 'body'});
+    var annoRange = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'quote', quote: 'test', ranges: quoteRange});
+  });
 
-var valueRange = annotations.createRange({exact: 'value exact', offset: 200});
-var annoValueQuote = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'valueQuote', key: 'test key', value: 'test value', ranges: valueRange});
-console.log('annoValueQuote', annoValueQuote);
+  it('should create valueQuote annotation', function() {
+    var valueRange = annotations.createRange({exact: 'value exact', offset: 200, selector: 'body'});
+    var annoValueQuote = annotations.createAnnotation({hasTarget: item.uri, annotatedBy: 'test', type: 'valueQuote', key: 'test key', value: 'test value', ranges: valueRange});
+  });
 
-var annoLib = require('../lib/annotateServices/annotateLib');
+  it('should create category annotation', function() {
+    var text = '<html><body>This is a match1 and a match2. Here is match1 again.</body></html>';
+    var ranges = annoLib.rangesFromMatches('match1', text, 'body');
 
-var text = '<html><body>This is a match1 and a match2. Here is match1 again.</body></html>';
-var ranges = annoLib.rangesFromMatches('match1', text);
-
-console.log('range', ranges);
+  });
+});
