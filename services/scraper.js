0aa00c7cfaca3b038ef3e16bb396046dd4c6eaf4
Vid <vid_github@zooid.org>
2014-06-08 10:25:31 -0400

    organizing libs

--- services/scraper.js
+++ services/scraper.js
@@ -1,26 +1,9 @@
 // service that periodically checks for and requests queued content
 
-var scraper = require('./scraper.js'), content = require('./content.js'), utils = require('./utils.js');
-
 GLOBAL.config = require('../config.js').config;
+GLOBAL.config.indexer = require('../lib/indexer.js');
 
-setInterval(function() { scraper.getQueuedLink(getLink)}, 2000);
+var scraper = require('../lib/scraper.js');
 
-function getLink(err, queuedLink) {
-  console.log('scraping', queuedLink);
-  // retrieve https directly
-  if (queuedLink.uri.toString().indexOf('https') === 0) {
-    utils.retrieveHTTPS(queuedLink.uri, function(err, data) {
-      content.indexContentItem({ uri: queuedLink.uri, content: data, member: utils.localUser.username}, function(err, res) {
-        utils.passingError(err);
-        console.log('https scraped', err, data ? data.length : 'no content');
-      });
-    });
-  } else {
-    // use the proxy
-    scraper.scrapeLink(queuedLink.uri, function(err, res) {
-      console.log('scraped', err, res ? res.length : 'no content');
-    });
-  }
-}
-
+// watch for new links every 2 seconds
+setInterval(function() { scraper.getQueuedLink(scraper.getLink)}, 2000);
