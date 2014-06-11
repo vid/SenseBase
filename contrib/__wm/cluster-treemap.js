23c4fbd8ec48511c7f1e61d7e5001103bfe7fbba
Vid <vid_github@zooid.org>
2014-06-11 18:15:02 -0400

    checkbox selected cluster items

--- contrib/__wm/cluster-treemap.js
+++ contrib/__wm/cluster-treemap.js
@@ -1,17 +1,29 @@
+// treemap element has been selected
+// select document table items
+function selectCluster(d) { 
+  var docs = d3.event.target.__data__.documents;
+  docs.forEach(function(uri) {
+    $('input[name=cb_' + encID(uri) + ']').prop('checked', 'true');
+  });
+  // update selected count
+  checkSelected();
+
+  console.log('MM', d3.event.target.__data__.label, docs);
+
+}
 function doTreemap(data) {
-  var displayData = [];
   data.forEach(function(d) {
-    displayData.push({ value : d.documents.length, name: d.label});
+    d.size = d.documents.length;
   });
 
-  // instantiate d3plus
+console.log(d3plus.viz);
   var visualization = d3plus.viz()
     .container("#treemap")  // container DIV to hold the visualization
-    .data(displayData)  // data to use with the visualization
+    .data(data)  // data to use with the visualization
     .type("tree_map")   // visualization type
-    .id("name")         // key for which our data is unique on
-    .text("name")       // key to use for display text
-    .size("value")      // sizing of blocks
+    .id("label")         // key for which our data is unique on
+    .text("label")       // key to use for display text
+    .size("size")      // sizing of blocks
     .draw();             // finally, draw the visualization!
-console.log(visualization); 
 }
+d3.select('#treemap').on('click', selectCluster);
