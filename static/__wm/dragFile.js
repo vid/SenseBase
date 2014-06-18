5444439cdda0aadd618287bfff91e4799b0246c5
Vid <vid_github@zooid.org>
2014-06-18 19:08:14 -0400

    reusable dnd

--- static/__wm/dragFile.js
+++ static/__wm/dragFile.js
@@ -1,93 +1,96 @@
 // adapated from http://html5demos.com/dnd-upload
 
-document.getElementById("uploadInfo").className = "";
+function setupDND(dropzone, postLoc) {
 
-var holder = document.getElementById('holder'),
-  tests = {
-    filereader: typeof FileReader != 'undefined',
-    dnd: 'draggable' in document.createElement('span'),
-    formdata: !!window.FormData,
-    progress: "upload" in new XMLHttpRequest
-  }, 
-  support = {
-    filereader: document.getElementById('filereader'),
-    formdata: document.getElementById('formdata'),
-    progress: document.getElementById('progress')
-  },
-  progress = document.getElementById('uploadprogress'),
-  fileupload = document.getElementById('upload');
+  document.getElementById("uploadInfo").className = "";
 
-["filereader", "formdata", "progress"].forEach(function (api) {
-console.log(api, support[api]);
-  if (tests[api] === false) {
-    support[api].className = 'fail';
-  } else {
-    support[api].className = 'hidden';
-  }
-});
+  var holder = document.getElementById(dropzone),
+    tests = {
+      filereader: typeof FileReader != 'undefined',
+      dnd: 'draggable' in document.createElement('span'),
+      formdata: !!window.FormData,
+      progress: "upload" in new XMLHttpRequest
+    }, 
+    support = {
+      filereader: document.getElementById('filereader'),
+      formdata: document.getElementById('formdata'),
+      progress: document.getElementById('progress')
+    },
+    progress = document.getElementById('uploadprogress'),
+    fileupload = document.getElementById('upload');
 
-// FIXME
-function previewfile(file) {
-  if (false && tests.filereader === true) {
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      var image = new Image();
-      image.src = event.target.result;
-      image.width = 250; // a fake resize
-      holder.appendChild(image);
-    };
-
-    reader.readAsDataURL(file);
-  }  else {
-    holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size/1024|0) + 'K' : '');
-    console.log(file);
-  }
-}
-
-function readfiles(files) {
-    var formData = tests.formdata ? new FormData() : null;
-    for (var i = 0; i < files.length; i++) {
-      if (tests.formdata) formData.append('file', files[i]);
-      previewfile(files[i]);
+  ["filereader", "formdata", "progress"].forEach(function (api) {
+  console.log(api, support[api]);
+    if (tests[api] === false) {
+      support[api].className = 'fail';
+    } else {
+      support[api].className = 'hidden';
     }
+  });
 
-    // now post a new XHR request
-    if (tests.formdata) {
-      var xhr = new XMLHttpRequest();
-      document.getElementById('uploadprogress').className = '';
+  if (tests.dnd) { 
+    holder.ondragover = function () { console.log('hi'); this.className += ' hover'; return false; };
+    holder.ondragend = function () { console.log('bye'); this.className = this.className.replace(' hover', ''); return false; };
+    holder.ondrop = function (e) {
+      this.className = '';
+      e.preventDefault();
+      readfiles(e.dataTransfer.files);
+    }
+  } else {
+    fileupload.className = 'hidden';
+    fileupload.querySelector('input').onchange = function () {
+      readfiles(this.files);
+    };
+  }
 
-      xhr.open('POST', '/upload');
-      xhr.onload = function() {
-        progress.value = progress.innerHTML = 100;
+  // FIXME display filetype icon
+  function previewfile(file) {
+    if (false && tests.filereader === true) {
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var image = new Image();
+        image.src = event.target.result;
+        image.width = 250; // a fake resize
+        holder.appendChild(image);
       };
 
-      if (tests.progress) {
-        xhr.upload.onprogress = function (event) {
-          if (event.lengthComputable) {
-            var complete = (event.loaded / event.total * 100 | 0);
-            progress.value = progress.innerHTML = complete;
-            if (complete === 100) {
-              document.getElementById('uploadprogress').className = 'hidden';
+      reader.readAsDataURL(file);
+    }  else {
+      holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size/1024|0) + 'K' : '');
+      console.log(file);
+    }
+  }
+
+  function readfiles(files) {
+      var formData = tests.formdata ? new FormData() : null;
+      for (var i = 0; i < files.length; i++) {
+        if (tests.formdata) formData.append('file', files[i]);
+        previewfile(files[i]);
+      }
+
+      // now post a new XHR request
+      if (tests.formdata) {
+        var xhr = new XMLHttpRequest();
+        document.getElementById('uploadprogress').className = '';
+
+        xhr.open('POST', postLoc);
+        xhr.onload = function() {
+          progress.value = progress.innerHTML = 100;
+        };
+
+        if (tests.progress) {
+          xhr.upload.onprogress = function (event) {
+            if (event.lengthComputable) {
+              var complete = (event.loaded / event.total * 100 | 0);
+              progress.value = progress.innerHTML = complete;
+              if (complete === 100) {
+                document.getElementById('uploadprogress').className = 'hidden';
+              }
             }
           }
         }
+
+        xhr.send(formData);
       }
-
-      xhr.send(formData);
-    }
-}
-
-if (tests.dnd) { 
-  holder.ondragover = function () { this.className = 'hover'; return false; };
-  holder.ondragend = function () { this.className = ''; return false; };
-  holder.ondrop = function (e) {
-    this.className = '';
-    e.preventDefault();
-    readfiles(e.dataTransfer.files);
   }
-} else {
-  fileupload.className = 'hidden';
-  fileupload.querySelector('input').onchange = function () {
-    readfiles(this.files);
-  };
 }
