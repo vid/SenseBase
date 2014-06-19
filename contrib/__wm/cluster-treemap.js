63afc53dd80f87ff43236f1a3cf73bce2484411f
Vid <vid_github@zooid.org>
2014-06-19 18:30:30 -0400

    modularize

--- contrib/__wm/cluster-treemap.js
+++ contrib/__wm/cluster-treemap.js
@@ -1,29 +1,31 @@
-// treemap element has been selected
-// select document table items
-function selectCluster(d) { 
-  var docs = d3.event.target.__data__.documents;
-  docs.forEach(function(uri) {
-    $('input[name=cb_' + encID(uri) + ']').prop('checked', 'true');
-  });
-  // update selected count
-  checkSelected();
+var browseCluster = {
 
-  console.log('MM', d3.event.target.__data__.label, docs);
+  // treemap element has been selected
+  // select document table items
+  selectCluster : function(d) { 
+    var docs = d3.event.target.__data__.documents;
+    docs.forEach(function(uri) {
+      $('input[name=cb_' + encID(uri) + ']').prop('checked', 'true');
+    });
+    // update selected count
+    checkSelected();
 
+    console.log('MM', d3.event.target.__data__.label, docs);
+
+  },
+  doTreemap : function(data) {
+    data.forEach(function(d) {
+      d.size = d.documents.length;
+    });
+
+    var visualization = d3plus.viz()
+      .container("#browse")  // container DIV to hold the visualization
+      .data(data)  // data to use with the visualization
+      .type("tree_map")   // visualization type
+      .id("label")         // key for which our data is unique on
+      .text("label")       // key to use for display text
+      .size("size")      // sizing of blocks
+      .draw();             // finally, draw the visualization!
+    d3.select('#browse').on('click', selectCluster);
+  }
 }
-function doTreemap(data) {
-  data.forEach(function(d) {
-    d.size = d.documents.length;
-  });
-
-console.log(d3plus.viz);
-  var visualization = d3plus.viz()
-    .container("#browse")  // container DIV to hold the visualization
-    .data(data)  // data to use with the visualization
-    .type("tree_map")   // visualization type
-    .id("label")         // key for which our data is unique on
-    .text("label")       // key to use for display text
-    .size("size")      // sizing of blocks
-    .draw();             // finally, draw the visualization!
-}
-d3.select('#browse').on('click', selectCluster);
