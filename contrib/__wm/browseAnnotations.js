019c49bce96e5c09e81c1473a7f059ed7584dc38
Vid <vid_github@zooid.org>
2014-06-21 08:52:42 -0400

    resize treemap

--- contrib/__wm/browseAnnotations.js
+++ contrib/__wm/browseAnnotations.js
@@ -1,7 +1,6 @@
 var browseAnnotations = {
   doTreemap : function(results, target) {
     $(target).addClass('treemap');
-    $(target).html('');
     var root = { name : 'Annotations', size: 1, children: []
     };
     // build a size of hierarchical annotations
@@ -33,90 +32,96 @@ var browseAnnotations = {
     });
     //$('#results').html('<pre>' + JSON.stringify(root, null, 2) + '</pre>');
 
+  renderTreemap(root, target);
+  window.onresize = function() { renderTreemap(root, target)};
+
+  function renderTreemap(root, target) {
+    $(target).html('');
     var $target = $(target), 
-       w = $target.innerWidth(),
+      w = $target.innerWidth(),
       h = $target.innerHeight(),
       x = d3.scale.linear().range([0, w]),
       y = d3.scale.linear().range([0, h]);
 
-  var vis = d3.select(target).append("div")
-      .attr("class", "chart")
-      .style("width", w + "px")
-      .style("height", h + "px")
-    .append("svg:svg")
-      .attr("width", w)
-      .attr("height", h);
+    var vis = d3.select(target).append("div")
+        .attr("class", "chart")
+        .style("width", w + "px")
+        .style("height", h + "px")
+      .append("svg:svg")
+        .attr("width", w)
+        .attr("height", h);
 
-  var div = d3.select("body").append('div')   
-    .attr("class", "tooltip")               
-    .style("opacity", 0);
+    var div = d3.select("body").append('div')   
+      .attr("class", "tooltip")               
+      .style("opacity", 0);
 
-  var partition = d3.layout.partition()
-      .value(function(d) { return d.size; });
+    var partition = d3.layout.partition()
+        .value(function(d) { return d.size; });
 
-    var g = vis.selectAll("g")
-        .data(partition.nodes(root))
-      .enter().append("svg:g")
-        .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; })
-        .on('mouseover', function(d) {
-          div.transition()        
-            .duration(200)      
-            .style("opacity", .9);      
-            div.html(d.name + ' (' + (d.children ? d.children.length + ' sub' : d.size + ' members') + ')')
-              .style("left", (d3.event.pageX) + "px")     
-              .style("top", (d3.event.pageY - 28) + "px");    
-          })          
-        .on('mouseout', function(d) {
-          div.transition()        
-            .duration(500)      
-            .style("opacity", 0);   
-        })
-        .on("click", click);
+      var g = vis.selectAll("g")
+          .data(partition.nodes(root))
+        .enter().append("svg:g")
+          .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; })
+          .on('mouseover', function(d) {
+            div.transition()        
+              .duration(200)      
+              .style("opacity", .9);      
+              div.html(d.name + ' (' + (d.children ? d.children.length + ' sub' : d.size + ' members') + ')')
+                .style("left", (d3.event.pageX) + "px")     
+                .style("top", (d3.event.pageY - 28) + "px");    
+            })          
+          .on('mouseout', function(d) {
+            div.transition()        
+              .duration(500)      
+              .style("opacity", 0);   
+          })
+          .on("click", click);
 
-    var kx = w / root.dx,
-      ky = h / 1;
+      var kx = w / root.dx,
+        ky = h / 1;
 
-    g.append("svg:rect")
-      .attr("width", root.dy * kx)
-      .attr("height", function(d) { return d.dx * ky; })
-      .attr("class", function(d) { return d.children ? "parent" : "child"; });
+      g.append("svg:rect")
+        .attr("width", root.dy * kx)
+        .attr("height", function(d) { return d.dx * ky; })
+        .attr("class", function(d) { return d.children ? "parent" : "child"; });
 
-    g.append("svg:text")
-      .attr("transform", transform)
-      .attr("dy", ".35em")
-      .style("opacity", function(d) { return d.dx * ky > 12 ? 1 : 0; })
-      .text(function(d) { return d.name; })
+      g.append("svg:text")
+        .attr("transform", transform)
+        .attr("dy", ".35em")
+        .style("opacity", function(d) { return d.dx * ky > 12 ? 1 : 0; })
+        .text(function(d) { return d.name; })
 
-/*
-    d3.select(window)
-      .on("click", function() { click(root); })
-*/
+  /*
+      d3.select(window)
+        .on("click", function() { click(root); })
+  */
 
-    function click(d) {
-      if (!d.children) return;
+      function click(d) {
+        if (!d.children) return;
 
-      kx = (d.y ? w - 40 : w) / (1 - d.y);
-      ky = h / d.dx;
-      x.domain([d.y, 1]).range([d.y ? 40 : 0, w]);
-      y.domain([d.x, d.x + d.dx]);
+        kx = (d.y ? w - 40 : w) / (1 - d.y);
+        ky = h / d.dx;
+        x.domain([d.y, 1]).range([d.y ? 40 : 0, w]);
+        y.domain([d.x, d.x + d.dx]);
 
-      var t = g.transition()
-          .duration(d3.event.altKey ? 7500 : 750)
-          .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; });
+        var t = g.transition()
+            .duration(d3.event.altKey ? 7500 : 750)
+            .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; });
 
-      t.select("rect")
-          .attr("width", d.dy * kx)
-          .attr("height", function(d) { return d.dx * ky; });
+        t.select("rect")
+            .attr("width", d.dy * kx)
+            .attr("height", function(d) { return d.dx * ky; });
 
-      t.select("text")
-          .attr("transform", transform)
-          .style("opacity", function(d) { return d.dx * ky > 12 ? 1 : 0; });
+        t.select("text")
+            .attr("transform", transform)
+            .style("opacity", function(d) { return d.dx * ky > 12 ? 1 : 0; });
 
-      d3.event.stopPropagation();
-    }
+        d3.event.stopPropagation();
+      }
 
-    function transform(d) {
-      return "translate(8," + d.dx * ky / 2 + ")";
+      function transform(d) {
+        return "translate(8," + d.dx * ky / 2 + ")";
+      }
     }
   }
 }
