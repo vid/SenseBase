3f0b01e353007e7bf4ffd98348f74c9e5526f6b5
Vid <vid_github@zooid.org>
2014-07-10 15:44:09 -0400

    adding bing web and news search

--- test/int/search-apis.js
+++ test/int/search-apis.js
@@ -4,9 +4,34 @@ var expect = require('expect.js');
 
 var searchAPIs = require('../../lib/search-apis.js');
 
+// FIXME required for test bing api key
+GLOBAL.config = require('../../config.js').config;
+
 describe('Search APIs', function(done) {
   it('should search entrez.esearch', function(done) {
-    searchAPIs.exec('entrez.esearch', {query: 'javascript', targetResults: 1}, function(err, uri, resultContext) {
+    var count = 0;
+    searchAPIs.exec('entrez.esearch', {query: 'javascript api', targetResults: 1}, function(err, uri, resultContext) {
+      expect(++count).to.be(1);
+      expect(err).to.be.null;
+      expect(uri).to.not.be.null;
+      expect(resultContext.referers).to.not.be.null;
+      done();
+    });
+  });
+  it('should search bing.web', function(done) {
+    var count = 0;
+    searchAPIs.exec('bing.web', {query: 'javascript api', targetResults: 1}, function(err, uri, resultContext) {
+      expect(++count).to.be(1);
+      expect(err).to.be.null;
+      expect(uri).to.not.be.null;
+      expect(resultContext.referers).to.not.be.null;
+      done();
+    });
+  });
+  it('should search bing.news', function(done) {
+    var count = 0;
+    searchAPIs.exec('bing.news', {query: 'javascript api', targetResults: 1}, function(err, uri, resultContext) {
+      expect(++count).to.be(1);
       expect(err).to.be.null;
       expect(uri).to.not.be.null;
       expect(resultContext.referers).to.not.be.null;
