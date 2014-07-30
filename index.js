8be0dca9d81d2beba02b802c5daa1e75f437fa48
Vid <vid_github@zooid.org>
2014-07-30 15:36:49 -0400

    more jshinting

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ exports.start = function(config, callback) {
       // FIXME don't save binaries
       GLOBAL.config.pageCache.cache(uri, referer, is_html, pageBuffer, contentType, saveHeaders, browser_request);
       var psMember = browser_request.psMember.username;
-      var desc = { uri: uri, referer: referer, isHTML: browser_request.is_html, content: pageBuffer, contentType: contentType, headers: saveHeaders}
+      var desc = { uri: uri, referer: referer, isHTML: browser_request.is_html, content: pageBuffer, contentType: contentType, headers: saveHeaders};
       content.indexContentItem(desc, { categories: ['proxy'], member: psMember});
     }
   };
