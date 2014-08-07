36ccd280adc2e075a4be47926b62edd71a421a92
Vid <vid_github@zooid.org>
2014-08-07 14:38:37 -0400

    break out search

--- util/entrez-esearch.js
+++ util/entrez-esearch.js
@@ -1,24 +1,27 @@
-// queues up pubmed pages based on an entrez esearch query
+// Queues up pubmed pages based on an entrez esearch query.
 // http://www.ncbi.nlm.nih.gov/books/NBK25497/
 /*jslint node: true */
 
 'use strict';
 
 GLOBAL.config = require(__dirname + '/../config.js').config;
-var utils = require('../lib/utils.js'), scraperLib = require('../lib/scraper');
+var utils = require('../lib/utils.js'), scraperLib = require('../lib/search');
 
 exports.queueEsearchResults = queueEsearchResults;
+exports.search = search;
 
-/*
-var annotator = process.argv[1].replace(/.*\//, '');
+// Allow command line searching.
+if (process.argv[2]) {
+  var term = process.argv[2], count = process.argv[3] || 1;;
+  console.log('queueing', term);
+  search({ terms: term, count: count}, function(err, res) {
+    console.log('err', err, 'res', JSON.stringify(JSON.parse(res), null, 2));
+  });
+}
 
-queueEsearchResults({ scraper: annotator, terms: 'ferritin', categories: ['boo', 'bar'], state: utils.states.annotations.validated, count: 5, relevance: 0, referers: []});
-*/
-
-// queue up terms with options. NB relevance on 0 means only the pages will be scraped (no links followed)
+// Queue up terms with options. NB relevance on 0 means only the pages will be scraped (no links followed).
 function queueEsearchResults(options) {
-  var queryURI = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=' + options.terms + '&retmode=json&retmax=' + (options.count || 10);
-  utils.retrieve(queryURI, function(err, res) {
+  retrieve(options, function(err, res) {
     if (err) {
       throw err;
     }
@@ -28,4 +31,10 @@ function queueEsearchResults(options) {
       scraperLib.queueLink(uri, (options.relevance || 0), options.categories, (options.referers || []), options.scraper, (options.state || utils.states.annotations.unvalidated));
     });
   });
+}
+
+// Do the actual search.
+function search(options, callback) {
+  var queryURI = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=' + options.terms + '&retmode=json&retmax=' + (options.count || 10);
+  utils.retrieve(queryURI, callback);
 }
