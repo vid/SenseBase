58166a3d52abc748587c7169e7df2f826e666e83
Vid <vid_github@zooid.org>
2014-08-29 18:10:45 -0400

    switch to later for scheduling

--- services/cron.js
+++ services/cron.js
@@ -5,14 +5,31 @@
 GLOBAL.config = require('../config.js').config;
 GLOBAL.config.indexer = require('../lib/indexer.js');
 
-var cron = require('cron');
+var _ = require('lodash'), later = require('later');
 
+var crons = [];
 checkCrons();
 
 function checkCrons() {
-  GLOBAL.config.indexer.retrieveSearches({}, function(err, res) {
-    console.log(err, JSON.stringify(res, null, 2));
-  });
-  setTimeout(checkCrons, 5000);
+  var i = crons.length;
+  for (i; i > 0; i--) {
+    delete crons[i];
+  }
 
+  GLOBAL.config.indexer.retrieveSearches({}, function(err, res) {
+    if (res.hits && res.hits.total > 0) {
+      _.pluck(res.hits.hits, '_source').forEach(function(search) {
+        if (search.cron) {
+          var secs = later.parse.cron(search.cron);
+          console.log('setting up', search.searchName, search.cron, secs);
+
+          // execute logTime one time on the next occurrence of the text schedule
+          var cron = later.setTimeout(function() {console.log(search);}, secs);
+
+          crons.push(cron);
+        }else{ console.log('no', search);}
+      });
+    }
+  });
+  setInterval(checkCrons, 5000);
 }
