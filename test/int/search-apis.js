b9f6870f046d0163f9e9dd87e67353b56a659c86
Vid <vid_github@zooid.org>
2014-09-03 17:27:08 -0400

    consolidating methods

--- test/int/search-apis.js
+++ test/int/search-apis.js
@@ -13,15 +13,15 @@ GLOBAL.config = require('../../config.js').config;
 describe('Search APIs', function(done) {
   it('should search entrez.esearch', function(done) {
     searchAPIs.exec('entrez.esearch', {query: 'javascript api', targetResults: 1}, function(err, uri, resultContext) {
-      expect(err).to.be(undefined);
+      expect(err).to.be(null);
       expect(uri).to.not.be(undefined);
-      expect(resultContext.referers).to.not.be(undefined);
+      expect(resultContext.referer).to.not.be(null);
       done();
     });
   });
   it('should search bing.web', function(done) {
     searchAPIs.exec('bing.web', {query: 'javascript api', targetResults: 1}, function(err, uri, resultContext) {
-      expect(err).to.be(undefined);
+      expect(err).to.be(null);
       expect(uri).to.not.be(undefined);
       expect(resultContext.referers).to.not.be(undefined);
       done();
@@ -29,7 +29,7 @@ describe('Search APIs', function(done) {
   });
   it('should search bing.news', function(done) {
     searchAPIs.exec('bing.news', {query: 'javascript api', targetResults: 1}, function(err, uri, resultContext) {
-      expect(err).to.be(undefined);
+      expect(err).to.be(null);
       expect(uri).to.not.be(undefined);
       expect(resultContext.referers).to.not.be(undefined);
       done();
