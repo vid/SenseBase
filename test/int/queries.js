8d284a0fe921bba21fc3caf0c1433eb13e058e98
Vid <vid_github@zooid.org>
2014-09-11 17:19:57 -0400

    filter on date with test

--- test/int/queries.js
+++ test/int/queries.js
@@ -7,7 +7,7 @@ var _ = require('lodash'), expect = require("expect.js");
 
 var annotations = require('../../lib/annotations.js'), testApp = require('../lib/test-app.js'), utils = require('../../lib/utils.js');
 
-var dateField, beforeDateQuery, afterDateQuery;
+var splitYear = new Date(2003, 5, 15), dateField, beforeDateQuery, afterDateQuery;
 
 describe('Queries', function(done) {
   it('should reset test app', function(done) {
@@ -26,20 +26,23 @@ describe('Queries', function(done) {
       queryName: GLOBAL.testing.uniq + '-before',
       member: GLOBAL.testing.uniqMember,
       annotations: ['tag0', 'tag1', 'tag2'],
-      filters: [ { type: 'value', position: dateField, operator: '<', value: new Date(2003, 5, 15)} ]
+      filters: [ { type: 'value', position: dateField, operator: '<', value: splitYear} ]
     };
     afterDateQuery = {
       queryName: GLOBAL.testing.uniq + '-after',
       member: GLOBAL.testing.uniqMember,
       annotations: ['tag3', 'tag4'],
       filters: [
-      { type: 'value', position: dateField, operator: '>', value: new Date(2003, 5, 15)} ]
+      { type: 'value', position: dateField, operator: '>', value: splitYear} ]
     };
 
-    var c, a, cItems = [], date, name = GLOBAL.testing.uniqMember, uri, year = 2000;
+    var c, a, cItems = [], date, name = GLOBAL.testing.uniqMember, uri, year;
+    // create cItems
     for (c = 0; c < 5; c++) {
+      year = 2000;
       uri = GLOBAL.testing.uniqURI + '-' + c;
       var cItem = annotations.createContentItem({title: 'test title', uri: uri, content: 'test content ' + GLOBAL.testing.uniq, annotations: []});
+      // with annotations
       for (a = 0; a < 5; a++) {
         date = new Date(year++, 5, 15);
         cItem.annotations.push(annotations.createAnnotation({type: 'value', isA: 'Date', annotatedBy: name, hasTarget: uri, key: 'testDate', value: date, roots: [GLOBAL.testing.uniq]}));
@@ -77,11 +80,18 @@ describe('Queries', function(done) {
     });
   });
 
-  it('should include multiple categories in a query', function(done) {
+  it('should perform a filtered query', function(done) {
     GLOBAL.svc.indexer.formQuery(beforeDateQuery, function(err, res) {
       expect(err).to.be(null);
       expect(res.hits.total).to.be(3);
-      _.pluck(res.hits.hits, '_source').forEach(function (cItem) {
+      var foundDates = 0;
+      _.where(res.hits.hits, { '_source': ['annotations']}).forEach(function(anno) {
+          if (anno.isA === 'Date') {
+            foundDates++;
+            expect(anno.typed.Date < splitYear);
+          }
+        console.log(foundDates);
+        expect(foundDates).to.be(15);
       });
       done();
     }, { sourceFields: GLOBAL.svc.indexer.sourceFields.concat(['annotations.*'])});
