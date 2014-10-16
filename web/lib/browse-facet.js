9b2f0e34e01fc5939098a907b108164272808b58
Vid <vid_github@zooid.org>
2014-10-16 10:40:13 -0400

    fix plugins for jstree

--- web/lib/browse-facet.js
+++ web/lib/browse-facet.js
@@ -5,30 +5,33 @@
 
 'use strict';
 
-var facetSearchTimeout, typesIcons = require('./types-icons.js');
+var facetSearchTimeout, typesIcons = require('./types-icons.js').types;
+var jstreeOpts = {
+  core : { },
+  plugins : ['search'],
+  search : { fuzzy: false, show_only_matches: true }
+};
 
 exports.render = function(target, results, resultView, context) {
   var $target = $(target), annos = augment(results.annotationOverview);
+  $target.css('font-size', '80%');
+  $target.jstree('destroy');
+  $target.html('');
+  jstreeOpts.core.data = annos;
+  console.log(jstreeOpts);
   $target
     .bind('loaded.jstree', function(event, data) {
-    data.instance.open_all();
-    window.dd = data;
-    $target.prepend('Filter: <input id="facetSearch">');
-    $('#facetSearch').keyup(function () {
-      if(facetSearchTimeout) { clearTimeout(facetSearchTimeout); }
+      window.dd = data;
+      $('#facetFilter').remove();
+      $target.prepend('<div id="facetFilter">Filter: <input id="facetSearch"></div>');
+      $('#facetSearch').keyup(function () {
+        if(facetSearchTimeout) { clearTimeout(facetSearchTimeout); }
         facetSearchTimeout = setTimeout(function () {
           var v = $('#facetSearch').val();
           $target.jstree(true).search(v);
         }, 250);
       });
-    }).jstree(
-      { core : {
-        data :  annos,
-      },
-      plugins : ['types', 'search'],
-      search : { fuzzy: false },
-      types : typesIcons.types
-    });
+    }).jstree(jstreeOpts);
   $target.on('select_node.jstree', function (e, data) {
     var i, j;
     for(i = 0, j = data.selected.length; i < j; i++) {
@@ -41,8 +44,11 @@ exports.render = function(target, results, resultView, context) {
 
 // add facet counts &c
 function augment(el) {
+  el.type = el.type || 'category';
+  el.icon = (typesIcons[el.type] || typesIcons.default).icon;
   if (el.children && el.children.length > 0) {
     el.text = el.text + ' (' + el.children.length + ')';
+    el.state = { opened: el.children.length > 100 ? false : true };
     el.children.forEach(function(c) {
       c = augment(c);
     });
