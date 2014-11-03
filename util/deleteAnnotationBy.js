62415b4ca3b502b83b46b7f4a6ca94254a7620c9
Vid <vid_github@zooid.org>
2014-11-03 14:28:37 -0500

    deleteAnnotationBy

--- util/deleteAnnotationBy.js
+++ util/deleteAnnotationBy.js
@@ -4,23 +4,44 @@
 
 require(process.cwd() + '/index.js').setup();
 
-var clientID = GLOBAL.svc.auth.clientIDByUsername('system');
+var contentLib = require('../lib/content');
+
+var member = 'system';
+
+var clientID = GLOBAL.svc.auth.clientIDByUsername(member);
 var _ = require('lodash');
 
 if (process.argv.length < 4) {
-  console.log('usage:', process.argv[1], 'annotation size [preview-only]');
+  console.log('usage:', process.argv[1], 'annotatedBy size|0 [preview-only]');
   process.exit(1);
 }
 
-var annotation = process.argv[2], size = process.argv[3], preview = process.argv[4];
+var by = process.argv[2], size = process.argv[3], preview = process.argv[4];
+var params = { query: { terms: 'annotatedBy:"' + by + '"' } };
+if (size) {
+  params.query.size = size;
+}
 
-GLOBAL.svc.indexer.formQuery({ query: annotation + flattened, size: size }, function(err, res) {
+GLOBAL.svc.indexer.formQuery(params, function(err, res) {
   if (err) {
     console.log('query error', err);
     process.exit(1);
   }
-  console.log('total result set', res.hits.total);
-  res.hits.hits.forEach(function(hit) {
-    var found = _.pluck(hit.annotations, { flattened: query});
-  });
+
+  console.log('result size', res.hits.hits.length, 'out of', res.hits.total);
+  if (preview) {
+    console.log(res.hits.hits.forEach(function(hit) {
+      console.log('hit', JSON.stringify(hit._source, null, 2));
+    }));
+  } else {
+    res.hits.hits.forEach(function(hit) {
+      contentLib.indexContentItem({ uri: hit._source.uri}, { member: member, deleteExistingAnnotatedBy: by}, function(err, res) {
+        if (err) {
+          console.log('failed:', err);
+        } else {
+          console.log('deleted', res);
+        }
+      });
+    });
+  }
 });
