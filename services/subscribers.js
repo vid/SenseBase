c40b896140287ee8120f819db7665ac1dcb758b7
Vid <vid_github@zooid.org>
2014-11-29 13:38:12 -0500

    change default sort to timestamp

--- services/subscribers.js
+++ services/subscribers.js
@@ -4,13 +4,23 @@
 
 require(process.cwd() + '/index.js').setup();
 
+var subscriptionLib = require('../lib/subscriptions');
+
 var clientID = GLOBAL.svc.auth.clientIDByUsername('system');
 var pubsub = require('../lib/pubsub-client').init({ homepage: GLOBAL.config.HOMEPAGE, clientID: clientID }), subscripionLib = require('../lib/subscriptions');
 var _ = require('lodash');
 
-// retrieve subscriptions
-pubsub.subscriptions.request({}, function(results) {
-  if (results.hits) {
-    var subscriptions = _.pluck(results.hits.hits, '_source');
-  }
-});
+// check on start
+processSubscriptions();
+// check every minute
+setInterval(processSubscriptions, 60000);
+
+// process any subscriptions in the last time period
+function processSubscriptions() {
+  pubsub.subscriptions.request({}, function(results) {
+    if (results.hits) {
+      var subscriptions = _.pluck(results.hits.hits, '_source');
+      console.log(subscriptions);
+    }
+  });
+}
