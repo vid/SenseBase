7ea33412f53484f378556c7be9fd9fd49d2b40f0
Vid <vid_github@zooid.org>
2014-11-29 16:29:23 -0500

    process and send subscription notifications via email

--- services/subscribers.js
+++ services/subscribers.js
@@ -1,26 +1,71 @@
-// service that periodically checks for and requests queued content
-/*jslint node: true */
+// service that fulfills subscriptions to updated content
+/* jslint node: true */
 'use strict';
 
 require(process.cwd() + '/index.js').setup();
 
-var subscriptionLib = require('../lib/subscriptions');
+
+var nodemailer = require('nodemailer'), _ = require('lodash');
+
+var transporter = nodemailer.createTransport(GLOBAL.config.mailer.transport), mailOptions = {
+  from: GLOBAL.config.mailer.from,
+  subject: GLOBAL.config.mailer.subject
+};
 
 var clientID = GLOBAL.svc.auth.clientIDByUsername('system');
-var pubsub = require('../lib/pubsub-client').init({ homepage: GLOBAL.config.HOMEPAGE, clientID: clientID }), subscripionLib = require('../lib/subscriptions');
-var _ = require('lodash');
+var utils = require('../lib/utils'), pubsub = require('../lib/pubsub-client').init({ homepage: GLOBAL.config.HOMEPAGE, clientID: clientID }), subscriptionLib = require('../lib/subscriptions');
 
-// check on start
-processSubscriptions();
-// check every minute
-setInterval(processSubscriptions, 60000);
+pubsub.item.subUpdated(processSubscriptions);
 
 // process any subscriptions in the last time period
-function processSubscriptions() {
-  pubsub.subscriptions.request({}, function(results) {
-    if (results.hits) {
-      var subscriptions = _.pluck(results.hits.hits, '_source');
-      console.log(subscriptions);
+function processSubscriptions(items) {
+  // queue up matches
+  // { "member" : { "uri" : [ "match", "match" ] } }
+  var toSend = {};
+  GLOBAL.svc.indexer.retrieveSubscriptions({}, function(err, res) {
+    if (res.hits) {
+      var subscriptions = _.pluck(res.hits.hits, '_source');
+      // for each hit
+      items.forEach(function(item) {
+        var matches = subscriptionLib.matches(item, subscriptions);
+        if (matches.length) {
+          matches.forEach(function(match) {
+            toSend[match.member] = toSend[match.member] || {};
+            toSend[match.member][item.uri] = toSend[match.member][item.uri] || [];
+            toSend[match.member][item.uri].push(match.match);
+          });
+        }
+      });
+      send(toSend);
     }
   });
+}
+
+function send(toSend) {
+  for (var member in toSend) {
+    GLOBAL.svc.auth.findByUsername(member, function(err, to) {
+      if (err) {
+        GLOBAL.error(err);
+      } else if (!to.email) {
+        GLOBAL.error('no member', member, to);
+      } else {
+        var text = '', html = '', tag = '\n--\n\n' + GLOBAL.config.HOMEPAGE;
+        for (var uri in toSend[member]) {
+          text += '\n' + GLOBAL.config.base + '?"' + uri + '"\n' + uri + ':\n' + toSend[member][uri].join('\n');
+          html += '<br /><a href="' + GLOBAL.config.HOMEPAGE + '?terms=%22' + uri + '%22">' + uri + '</a>\n<ul>' +
+            toSend[member][uri].map(function(a) { return '<li>'+a+'</li>'; }).join('') + '</ul>';
+        }
+        var message = _.extend(mailOptions, { to: to.email, text: text + tag, html: html + tag });
+
+        transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+            console.error('mailer failed', error);
+          } else {
+            console.log('Message sent: ' + info.response);
+          }
+        });
+        GLOBAL.debug('send to', to);
+      }
+    });
+  }
 }
