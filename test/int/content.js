ab10146d032f10fe1ca86c358b38fd1909d86063
Vid <vid_github@zooid.org>
2014-12-07 14:00:54 -0500

    adding ContentItem diff

--- test/int/content.js
+++ test/int/content.js
@@ -6,21 +6,21 @@
 var expect = require("expect.js");
 
 var content = require('../../lib/content.js'), annotations = require('../../lib/annotations.js'), testApp = require('../lib/test-app.js'), utils = require('../../lib/utils.js');
-var newTitle;
 
 describe('Content', function(done) {
   // used for history / version tests
-  var ongoing;
+  var initial, indexed, updated, initialTitle, updatedTitle;
   it('should reset test app', function(done) {
     testApp.start(function(err, ok) {
       expect(err).to.be(undefined);
-      newTitle = 'updated title ' + GLOBAL.testing.uniq;
+      initialTitle = 'test title ' + GLOBAL.testing.uniq;
+      updatedTitle = 'updated title ' + GLOBAL.testing.uniq;
       done();
     });
   });
 
   it('should index a queued page', function(done) {
-    var desc = annotations.createContentItem({ title: 'test title', uri: GLOBAL.testing.uniqURI, queued : { lastAttempt: new Date().toISOString() }} );
+    var desc = annotations.createContentItem({ title: initialTitle, uri: GLOBAL.testing.uniqURI, queued : { lastAttempt: new Date().toISOString() }} );
     content.indexContentItem(desc, {member: GLOBAL.testing.uniqMember, state: utils.states.content.queued}, function(err, res) {
       expect(err).to.be(null);
       expect(res._id).to.not.be(null);
@@ -35,18 +35,17 @@ describe('Content', function(done) {
   it('should retrieve the indexed page', function(done) {
     GLOBAL.svc.indexer.retrieveByURI(GLOBAL.testing.uniqURI, function(err, r) {
       expect(err).to.be(null);
-      var cItem = r._source;
-      expect(cItem).not.to.be(undefined);
-      expect(cItem.state).to.be(utils.states.content.queued);
-      ongoing = cItem;
+      initial = r._source;
+      expect(initial).not.to.be(undefined);
+      expect(initial.state).to.be(utils.states.content.queued);
       done();
     });
   });
 
 // content & state
   it('should index an updated page with content', function(done) {
-    ongoing.content = 'test content ' + GLOBAL.testing.uniq;
-    content.indexContentItem(ongoing, {member: GLOBAL.testing.uniqMember}, function(err, res) {
+    initial.content = 'test content ' + GLOBAL.testing.uniq;
+    content.indexContentItem(initial, {member: GLOBAL.testing.uniqMember}, function(err, res) {
       expect(err).to.be(null);
       done();
     });
@@ -59,17 +58,17 @@ describe('Content', function(done) {
   it('should retrieve the updated page with updated content', function(done) {
     GLOBAL.svc.indexer.retrieveByURI(GLOBAL.testing.uniqURI, function(err, r) {
       expect(err).to.be(null);
-      var cItem = r._source;
-      expect(cItem).to.not.be(undefined);
-      expect(cItem.state).to.be(utils.states.content.visited);
+      indexed = r._source;
+      expect(indexed).to.not.be(undefined);
+      expect(indexed.state).to.be(utils.states.content.visited);
       done();
     });
   });
 
 // title
   it('should index an updated page title', function(done) {
-    ongoing.title = newTitle;
-    content.indexContentItem(ongoing, {member: GLOBAL.testing.uniqMember}, function(err, res) {
+    indexed.title = updatedTitle;
+    content.indexContentItem(indexed, {member: GLOBAL.testing.uniqMember}, function(err, res) {
       expect(err).to.be(null);
       done();
     });
@@ -82,11 +81,20 @@ describe('Content', function(done) {
   it('should retrieve the updated page with updated title', function(done) {
     GLOBAL.svc.indexer.retrieveByURI(GLOBAL.testing.uniqURI, function(err, r) {
       expect(err).to.be(null);
-      var cItem = r._source;
-      expect(cItem).to.not.be(undefined);
-      expect(cItem.title).to.equal(newTitle);
+      updated = r._source;
+      expect(updated).to.not.be(undefined);
+      expect(updated.title).to.equal(updatedTitle);
       done();
     });
+  });
+
+  it('should find the difference in the changed versions', function(done) {
+    var diff = content.diffContentItems(updated, updated);
+    expect(diff).to.be(undefined);
+    diff = content.diffContentItems(initial, updated);
+    expect(diff).to.not.be(undefined);
+    expect(diff.length).to.eql(6);
+    done();
   });
 
 });
