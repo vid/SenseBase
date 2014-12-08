808a7f9d117340c5981f41a2e2d54a91c6d8e365
Vid <vid_github@zooid.org>
2014-12-08 00:40:52 -0500

    clean up output

--- web/lib/results.table.js
+++ web/lib/results.table.js
@@ -66,14 +66,14 @@ exports.render = function(dest, res, context) {
     }
     return false;
   };
-  var t = '<table id="resultsTable" class="ui sortable table"><thead><tr><th class="descending">Rank</th><th>Title</th><th>Visitors</th><th>Annotations</th>';
+  var t = '<table id="resultsTable" class="ui sortable table"><thead><tr><th class="descending">Rank</th><th>Title</th>';
   if (options.query.selectFields) {
     options.query.selectFields.forEach(function(f) {
       var type = f.replace(/␟.*/, '');
       t += '<th><i class="icon ' + (type === 'category' ? 'tag' : 'info') + '"></i>' + f.replace(/.*␟/, '') + '</th>';
     });
   }
-  $(dest).html(t + '</tr></thead><tbody></tbody></table>');
+  $(dest).html(t + '<th>Visitors</th><th>Annotations</th></tr></thead><tbody></tbody></table>');
   var count = 0;
   results.hits.hits.forEach(function(r) {
     var v = r.fields || r._source, highlight = '';
@@ -84,7 +84,26 @@ exports.render = function(dest, res, context) {
     var row = '<tr class="selectRow" id="' + utils.encID(v.uri) + '"><td data-sort-value="' + rankVal + '"><input class="selectItem" type="checkbox" name="cb_' + utils.encID(v.uri) + '" />' + rankVal + '</td><td>' +
       '<div><a class="selectURI title" href="'+ v.uri + '">' + (v.title ? v.title : '(no title)') + '</a><br />' +
       '<a class="selectURI content"><i class="text file icon"></i></a> <a class="selectURI uri" href="'+ v.uri + '"> ' + utils.shortenURI(v.uri) + '</a></div><div class="highlighted">' + highlight +
-'</div></td><td class="rowVisitors" data-sort-value="' + (v.visitors ? v.visitors.length : 0) + '">';
+'</div></td>';
+
+    if (options.query.selectFields) {
+      var span = '<span class="ui small label" style="text-transform: none">';
+      options.query.selectFields.forEach(function(f) {
+        var values = [];
+        (v.fields || []).forEach(function(v) {
+          if (v.flattened.indexOf(f) === 0) {
+            var val = v.value || (v.category ? v.category[v.category.length - 1] : '');
+            if (val) {
+              values.push(val);
+            }
+          }
+        });
+        row += '<td data-sort-value="' + values[0] + '">' + (values.length > 0 ? (span + values.join('</span> ' + span) + '</span>') : '') + '</td>';
+      });
+    }
+
+    // visitors
+    row += '<td class="rowVisitors" data-sort-value="' + (v.visitors ? v.visitors.length : 0) + '">';
     // roll up visitors
     if (v.visitors) {
       var vv = '', va = {};
@@ -98,7 +117,10 @@ exports.render = function(dest, res, context) {
       }
       row += '' + vv;
     }
-    row += '</td><td class="rowAnnotations">';
+    row += '</td>';
+
+    // annotation summary
+    row += '<td class="rowAnnotations">';
     if (v.annotationSummary !== undefined) {
       if (v.annotationSummary.validated > 0) {
         row += '<div class="ui tiny green annotations button"><i class="checked checkbox icon"></i> ' + v.annotationSummary.validated + '</div><div class="hidden validatedSummary"></div>';
@@ -108,19 +130,7 @@ exports.render = function(dest, res, context) {
       }
     }
     row += '</td>';
-    var span = '<span class="ui small label" style="text-transform: none">';
-    if (options.query.selectFields) {
-      options.query.selectFields.forEach(function(f) {
-        var values = [];
-        (v.fields || []).forEach(function(v) {
-          if (v.flattened.indexOf(f) === 0) {
-            var val = v.value || (v.category ? v.category[v.category.length - 1] : '');
-            values.push(val);
-          }
-        });
-        row += '<td data-sort-value="' + values[0] + '">' + (values.length > 0 ? (span + values.join('</span> ' + span) + '</span>') : '') + '</td>';
-      });
-    }
+
     $('#resultsTable tbody').append(row + '</tr>');
   });
 
