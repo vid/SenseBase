04e5721f77ec10f997afc321be5887744375aeaf
Vid <vid_github@zooid.org>
2015-01-17 17:46:41 -0500

    replace inbuilt diff with deep-diff

--- test/unit/content.js
+++ test/unit/content.js
@@ -27,7 +27,7 @@ describe('Content', function() {
 
   describe('should replaceThenMergeAnnotations', function() {
     it('should not process needsValidation true', function() {
-      var rannos = contentLib.replaceThenMergeAnnotations(_.clone(annos), _.clone(nannos));
+      var rannos = contentLib.replaceThenMergeAnnotations(_.cloneDeep(annos), _.cloneDeep(nannos));
       expect(rannos.length).to.be(3);
       expect(rannos[1].value).to.be('test value 2');
       expect(rannos[1].state).to.be(unvalidated);
@@ -37,7 +37,7 @@ describe('Content', function() {
       GLOBAL.svc.auth.getUserByUsername = function(annotatedBy) {
         return { needsValidation: false };
       };
-      var rannos = contentLib.replaceThenMergeAnnotations(_.clone(annos), _.clone(nannos));
+      var rannos = contentLib.replaceThenMergeAnnotations(_.clone(annos), _.cloneDeep(nannos));
       expect(rannos.length).to.be(3);
       expect(rannos[1].value).to.be('test value 2');
       expect(rannos[1].state).to.be(validated);
@@ -47,50 +47,39 @@ describe('Content', function() {
   describe('should compare content', function() {
     var ci = annoLib.createContentItem({ title: 'original title', uri: 'http://test.com', queued : { lastAttempt: '2015-01-17T21:18:35.189Z' }} );
 
-    var original = {
-      title: 'original title ',
-      uri: 'http://test.com/',
-      headers: {},
-      created: '2015-01-17T21:18:35.198Z',
-      annotations: [],
-      visitors: [
-        {
-          member: 'member14af9c3021bxc8fa4806',
-          mode: 'browser',
-          timestamp: '2015-01-17T21:18:37.254Z'
-        }
-      ],
-      referers: [],
-      state: 'visited',
-      queued: {
-        lastAttempt: '2015-01-17T21:18:35.189Z'
-      },
-      content: 'test content',
-      _id: 'http%3A%2F%2Ftest.com%2F',
-      timestamp: '2015-01-17T21:18:37.254Z',
-      sha1: '5d823dc17caabba4cb8f82e8bf24a991ee8b1b78',
-      text: 'test content ',
-      annotationSummary: {}
-    };
-
-    var updated = _.clone(original);
-
     it('should find no difference in an unchanged versions', function(done) {
-      var diff = contentLib.diffContentItems(original, original);
-      console.log(diff);
+      var original = _.cloneDeep(ci), same = _.cloneDeep(ci);
+      var diff = contentLib.diffContentItems(original, same);
       expect(diff).to.be(undefined);
       done();
     });
     it('should find no difference with a new visitor', function(done) {
-      var diff = contentLib.diffContentItems(original, ci);
-      console.log(diff);
+      var original = _.cloneDeep(ci), visited = _.cloneDeep(ci);
+      visited.visitors.push({ member: "test", mode: "test", timestamp: "2015-01-17T16:27:48.467Z" });
+      var diff = contentLib.diffContentItems(original, visited);
       expect(diff).to.be(undefined);
       done();
     });
+    it('should find a difference with an added annotation', function(done) {
+      var original = _.cloneDeep(ci), added = _.cloneDeep(ci);
+      added.annotations.push(annoLib.createAnnotation({hasTarget: 'http://test', annotatedBy: 'test', type: 'value', key: 'test key', value: 'test value 1', state: unvalidated}));
+
+      var diff = contentLib.diffContentItems(original, added);
+      expect(diff).to.not.be(undefined);
+      expect(diff.length).to.be(1);
+      expect(diff[0].kind).to.equal('A');
+      expect(diff[0].path).to.eql(['annotations']);
+      done();
+    });
     it('should find a difference with a changed annotation', function(done) {
-      var diff = contentLib.diffContentItems(original, original);
-      console.log(diff);
-      expect(diff).to.be(undefined);
+      var original = _.cloneDeep(ci), changed = _.cloneDeep(ci);
+      original.annotations.push(annoLib.createAnnotation({hasTarget: 'http://test', annotatedBy: 'test', type: 'value', key: 'test key', value: 'test value 1', state: unvalidated}));
+      changed.annotations.push(annoLib.createAnnotation({hasTarget: 'http://test', annotatedBy: 'test', type: 'value', key: 'test key', value: 'test value 1', state: validated}));
+      var diff = contentLib.diffContentItems(original, changed);
+      expect(diff).to.not.be(undefined);
+      expect(diff.length).to.be(1);
+      expect(diff[0].kind).to.equal('E');
+      expect(diff[0].path).to.eql([ 'annotations', 0, 'state' ]);
       done();
     });
   });
